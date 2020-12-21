fc111f6b375c0a463708c56fe4ae66e4f41256bc
nicolaspenagos <nicolas.penagosm98@gmail.com>
2020-12-21 02:46:55 -0500

    Some references have been added

--- app/src/main/java/com/example/parkingapp/LoginActivity.java
+++ app/src/main/java/com/example/parkingapp/LoginActivity.java
@@ -9,11 +9,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Window;
+import android.widget.Button;
+import android.widget.EditText;
 
 /*
  * This class is responsible for logging in each user that has already been registered in the database by the admin.
  */
 public class LoginActivity extends AppCompatActivity {
+
+    // -------------------------------------
+    // XML references
+    // -------------------------------------
+    private EditText emailEditText;
+    private EditText passwordEditText;
+    private Button loginButton;
 
     // -------------------------------------
     // Android methods
@@ -24,6 +33,11 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+
+        emailEditText = findViewById(R.id.emailEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        loginButton = findViewById(R.id.loginButton);
+
 
     }
 }
